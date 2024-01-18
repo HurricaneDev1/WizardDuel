cf2f70823a486d1bf2aff1050b28d499c7350d9d
HurricaneDev1 <82486522+HurricaneDev1@users.noreply.github.com>
2024-01-18 15:13:23 -0700

    Summon in players. Fix Borders
    Players spawn in. I fixed the borders not showing up

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,7 +6,9 @@ using UnityEngine.InputSystem;
 public class PlayerManager : MonoBehaviour
 {
     public static PlayerManager I;
-    public List<PlayerInput> playerInputs = new List<PlayerInput>();
+    [SerializeField]private PlayerInput playerInput;
+    [SerializeField]private Material player2;
+    private int numKeyboardPlayers;
     private int numPlayers;
     private List<PlayerInput> spawnedPlayers = new List<PlayerInput>();   
     void Awake(){
@@ -18,22 +20,32 @@ public class PlayerManager : MonoBehaviour
     }
 
     private void PlayerJoined(){
-        if(Keyboard.current.spaceKey.wasPressedThisFrame){
-            PlayerInput player = null;
-            switch(numPlayers){
-                case 0:
-                    player = Instantiate(playerInputs[0], transform.position, Quaternion.identity);
-                    player.defaultControlScheme = "Keyboard";
-                    player.SwitchCurrentControlScheme("Keyboard", Keyboard.current, Mouse.current);
-                    numPlayers ++;
-                    break;
-                case 1:
-                    player = Instantiate(playerInputs[0], transform.position, Quaternion.identity);
-                    player.defaultControlScheme = "Arrows";
-                    player.SwitchCurrentControlScheme("Arrows", Keyboard.current, Mouse.current);
-                    numPlayers ++;
-                    break;
-            }
+        PlayerInput player = null;
+        if(Keyboard.current.anyKey.wasPressedThisFrame && numKeyboardPlayers == 0){
+            SetControlsKeyboard("Keyboard", player);
+        }else if(ArrowPressed() && numKeyboardPlayers == 1){
+            SetControlsKeyboard("Arrows", player);
         }
+        // else if(Gamepad.current.aButton.wasPressedThisFrame){
+        //     player = Instantiate(playerInput, transform.position, Quaternion.identity);
+        //     player.defaultControlScheme = "Controller";
+        //     player.SwitchCurrentControlScheme("Controller", Gamepad.current);
+        //     numPlayers ++;
+        // }
+    }
+
+    public void SetControlsKeyboard(string nameOfControl, PlayerInput player){
+        player = Instantiate(playerInput, transform.position, Quaternion.identity);
+        player.defaultControlScheme = nameOfControl;
+        player.SwitchCurrentControlScheme(nameOfControl, Keyboard.current, Mouse.current);
+        numPlayers ++;
+        numKeyboardPlayers ++;
+        if(numKeyboardPlayers == 2){
+            player.GetComponentInChildren<SpriteRenderer>().material = player2;
+        }
+    }
+
+    bool ArrowPressed(){
+        return Keyboard.current.leftArrowKey.wasPressedThisFrame || Keyboard.current.rightArrowKey.wasPressedThisFrame || Keyboard.current.upArrowKey.wasPressedThisFrame || Keyboard.current.downArrowKey.wasPressedThisFrame; 
     }
 }
